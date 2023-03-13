423d0f94c006057071607e163be3503747a40aed
Mariusz Wiśniowski <108862878+marwisni@users.noreply.github.com>
2023-03-13 15:20:07 +0100

    Added environment variables.

--- webdata_saver/__init__.py
+++ webdata_saver/__init__.py
@@ -10,9 +10,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     content = requests.get(req.params.get('url')).content
     filename = f"bus_location@{datetime.strftime(datetime.now(), '%Y-%m-%d@%H-%M-%S')}.csv"
-    connection_string = os.environ["connection_string"]
     
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)        
-    blob_client = blob_service_client.get_blob_client(container='webfiles', blob=filename)    
+    blob_service_client = BlobServiceClient.from_connection_string(os.environ["STORAGE_CONNECTION_STRING"])        
+    blob_client = blob_service_client.get_blob_client(container=os.environ["STORAGE_CONTAINER_NAME"], blob=filename)    
     blob_client.upload_blob(content)
     return func.HttpResponse("File saved.")
