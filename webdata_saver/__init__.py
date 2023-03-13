0da733b52aa26a5d91cd71d9617857f8f4909fdd
Mariusz Wiśniowski <108862878+marwisni@users.noreply.github.com>
2023-03-13 22:06:43 +0100

    File name changed.

--- webdata_saver/__init__.py
+++ webdata_saver/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     content = requests.get(req.params.get('url')).content
-    filename = f"bus_location@{datetime.strftime(datetime.now(), '%Y-%m-%d@%H-%M-%S')}.csv"
+    filename = f"vehicles_locations@{datetime.strftime(datetime.now(), '%Y-%m-%d@%H-%M-%S')}.csv"
     
     blob_service_client = BlobServiceClient.from_connection_string(os.environ["STORAGE_CONNECTION_STRING"])        
     blob_client = blob_service_client.get_blob_client(container=os.environ["STORAGE_CONTAINER_NAME"], blob=filename)    
